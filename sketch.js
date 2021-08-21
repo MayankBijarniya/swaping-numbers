a1f844847159124c23490c42883000c664b19ea1
MayankBijarniya <86836326+MayankBijarniya@users.noreply.github.com>
2021-08-21 15:30:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+
+var a = prompt('Enter the first variable: ');
+var b = prompt('Enter the second variable: ');
+function setup(){
+  var b2 = createButton("click here to Swap");
+ b2.mousePressed(Swap);
+  }
+
+
+function draw()
+{
+}
+function Swap(){
+  [a,b]=[b,a];
+  console.log("The value of a after swaping",+a);
+  console.log("The value of b after swaping",+b);
+}
